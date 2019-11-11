45beed687a8c935ff9a6901b1da79e27cdb4b038
Zhen Data <zhenyang.9261@gmail.com>
2019-11-10 23:06:45 -0500

    concert-this option partially done. Not parse response object yet

--- liri.js
+++ liri.js
@@ -1,2 +1,66 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+var axios = require("axios");
+
+function concertThis(artist) {
+    var query = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    console.log(query);
+
+    axios
+      .get(query)
+      .then(function(response) {
+
+        console.log(response.data);
+      })
+      .catch(function(error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      });
+}
+
+/*
+ * Function: to show instructions of how to use this app. This function is called when user input is invalid
+ */
+function instruction() {
+
+    console.log("----------- How to user this app ----------");
+    console.log("node liri.js concert-this <artist name>");
+    console.log("node liri.js spotify-this-song <song name>");
+    console.log("node liri.js movie-this <movie name>");
+    console.log("node liri.js do-what-it-says");
+}
+
+//------------- Main starts here -------------------------
+var arg = process.argv;
+
+// User did not enter any option.
+if (arg[2] == null) {
+    instruction();
+    return;
+}
+
+var param = arg.slice(3).join("+")
+switch (arg[2].toLowerCase()) {
+    
+    case "concert-this":
+       
+        concertThis(param);
+        break;
+
+    default:
+        // User entered an invalid option
+        instruction();
+        break;
+}
